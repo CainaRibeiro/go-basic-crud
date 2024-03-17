371489e8a53e1d7614408b7227014c796d8c22e2
CainaRibeiro <cainaribeiro.it@outlook.com>
2024-03-17 16:53:03 -0300

    feat: controller

--- users/usersController.go
+++ users/usersController.go
@@ -1,12 +1,91 @@
 package users
 
 import (
-	"fmt"
+	"basic-mongo-crud/helpers"
+	"encoding/json"
+	"github.com/gorilla/mux"
 	"net/http"
 )
 
-func GetUsers(h http.ResponseWriter, r *http.Request) {
+var (
+	user Users
+	cpf  UserCpf
+	un   UserName
+)
 
-	h.WriteHeader(200)
-	fmt.Println("GETTING USERS...")
+func GetUsers(w http.ResponseWriter, r *http.Request) {
+	users, _ := getUsers()
+
+	jsonData, _ := json.Marshal(&users)
+	_, err := w.Write(jsonData)
+
+	if err != nil {
+		http.Error(w, "error writing response", http.StatusInternalServerError)
+	}
+	w.WriteHeader(http.StatusOK)
+}
+
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+	u, err := helpers.DecoderJSON(r, &user)
+	if err != nil {
+		panic(err)
+	}
+	newUser, _ := u.(*Users)
+
+	err = BodyValidator(newUser)
+	if err != nil {
+		http.Error(w, "Failed to create user", http.StatusInternalServerError)
+		return
+	}
+
+	CreateUsers(*newUser)
+
+	w.WriteHeader(http.StatusCreated)
+}
+
+func RemoveUser(w http.ResponseWriter, r *http.Request) {
+	c, err := helpers.DecoderJSON(r, &cpf)
+
+	if err != nil {
+		http.Error(w, "error decoding body", http.StatusBadRequest)
+	}
+
+	cpfUser := c.(*UserCpf)
+
+	s, e := DeleteUser(*cpfUser)
+	if e != nil {
+		http.Error(w, "failed to delete user", http.StatusInternalServerError)
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(s))
+}
+
+func UpdateUser(w http.ResponseWriter, r *http.Request) {
+	vr := mux.Vars(r)
+	id := vr["id"]
+
+	_, err := helpers.DecoderJSON(r, &un)
+
+	if err != nil {
+		http.Error(w, "error decoding json", http.StatusInternalServerError)
+	}
+
+	uu, err := UpdateUsersName(id, un.Name)
+	if err != nil {
+		http.Error(w, "error updating user", http.StatusInternalServerError)
+	}
+
+	jd, err := json.Marshal(&uu)
+
+	if err != nil {
+		http.Error(w, "error when marshaling json", http.StatusInternalServerError)
+	}
+
+	_, err = w.Write(jd)
+
+	if err != nil {
+		http.Error(w, "error writing response", http.StatusInternalServerError)
+	}
+	w.WriteHeader(http.StatusOK)
 }
